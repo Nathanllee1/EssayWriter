0dd6fb1c6f2f9a2016a39b8209af8875432654a7
Nathan Lee <36134754+Nathanllee1@users.noreply.github.com>
2018-11-11 22:04:06 -0800

    Trained another model + tweaks

--- train.py
+++ train.py
@@ -1,4 +1,3 @@
-
 import numpy as np
 import pandas as pd
 from keras.models import Sequential
@@ -38,14 +37,14 @@ Y_modified = np_utils.to_categorical(y)
 
 # set up a model
 model = Sequential()
-model.add(LSTM(400, input_shape=(X_modified.shape[1], X_modified.shape[2]), return_sequences=True))
+model.add(LSTM(700, input_shape=(X_modified.shape[1], X_modified.shape[2]), return_sequences=True))
 model.add(Dropout(0.2))
-model.add(LSTM(400))
+model.add(LSTM(700))
 model.add(Dropout(0.2))
 model.add(Dense(Y_modified.shape[1], activation='softmax'))
 model.compile(loss='categorical_crossentropy', optimizer='adam')
 
-model.fit(X_modified, Y_modified, epochs=50, batch_size=50)
+model.fit(X_modified, Y_modified, epochs=5, batch_size=50)
 
 #save weights
 model.save_weights(r'C:\Users\Nathan Lee\Workspace\EssayWriter\models\2.h5')
@@ -55,10 +54,24 @@ model.load_weights(r'C:\Users\Nathan Lee\Workspace\EssayWriter\models\2.h5')
 
 string_mapped = x[99]
 # generating characters
-for i in range(seq_length):
+
+string_mapped = X[99]
+full_string = [n_to_char[value] for value in string_mapped]
+# generating characters
+for i in range(400):
     x = np.reshape(string_mapped,(1,len(string_mapped), 1))
     x = x / float(len(characters))
+
     pred_index = np.argmax(model.predict(x, verbose=0))
     seq = [n_to_char[value] for value in string_mapped]
+    full_string.append(n_to_char[pred_index])
+
     string_mapped.append(pred_index)
     string_mapped = string_mapped[1:len(string_mapped)]
+
+
+txt=""
+for char in full_string:
+    txt = txt+char
+
+print(txt)
